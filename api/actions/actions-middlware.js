577e724c59b6ee660a182370b232c765e7a31709
Brian Bellinger <brianpatrickbellinger@gmail.com>
2021-12-03 00:22:15 -0800

    new actin post endpoint working

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,52 @@
 // add middlewares here related to actions
+const Action = require('./actions-model')
+
+async function validateId(req, res, next) {
+    Action.get(req.params.id)
+        .then(p => {
+            if (!p) {
+                res.status(404).json({ message: 'nothing found'})
+            } else {
+                next()
+            }
+        })
+        .catch(next)
+}
+
+function validateNewAction(req, res, next) {
+    const { notes, description, project_id } = req.body
+    
+    if (!project_id) {
+        res.status(400).json({ message: 'please include a project id'})
+    } else {
+        Action.get(project_id)
+        .then( proj => { //eslint-disable-line no-unused-vars
+             if (!description 
+                // || description.length() > 128 
+                ) {
+                res.status(400).json({ message: 'description field required, must be 128 chars or less' })
+            } else if (!notes) {
+                res.status(400).json({ message: 'notes field required'})
+            } else {
+                next()
+            }
+        })
+        .catch(err => {
+            res.status(404).json({ message: err.message })
+        })    
+    }
+}
+
+// eslint-disable-next-line no-unused-vars
+function errorHandling(err, req, res, next) { 
+    res.status(err.status || 500).json({
+        message: `${err.message}`,
+        stack: err.stack
+    })
+}
+
+module.exports = {
+    validateId,
+    validateNewAction,
+    errorHandling,
+}
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,11 +1,66 @@
 // Write your "actions" router here!
 const express = require('express')
 const Action = require('./actions-model')
+const {
+    validateId,
+    validateNewAction,
+    errorHandling,
+} = require('./actions-middlware')
+
 const router = express.Router()
 
+router.get('/', (req, res, next) => {
+    Action.get()
+        .then(actions => {
+            res.json(actions)
+        })
+        .catch(next)
+})
 
+router.get('/:id', validateId, (req, res, next) => {
+    Action.get(req.params.id)
+        .then(action => {
+            res.json(action)
+        })
+        .catch(next)
+})
 
+router.post('/', validateNewAction, (req, res, next) => {
+    console.log(req.body)
+    
+    Action.insert(req.body)
+        .then(action => {
+            res.status(201).json(action)
+        })
+        .catch(next)
+})
 
+router.put('/:id', validateId, (req, res, next) => {
+    
+    // Project.update(req.params.id, req.body)
+    //     .then(update => {
+    //         res.status(201).json(update)
+    //     })
+    //     .catch(next)
+})
+
+router.delete('/:id', validateId, (req, res, next) => {
+    // Project.remove(req.params.id)
+    //     .then(res => {
+    //         res.status(204)
+    //     })
+    //     .catch(next)
+})
+
+router.get('/:id/actions', validateId, (req, res, next) => {
+    // Project.getProjectActions(req.params.id)
+    //     .then(list => {
+    //         res.json(list)
+    //     })
+    //     .catch(next)
+})
+
+router.use(errorHandling)
 
 
 module.exports = router
